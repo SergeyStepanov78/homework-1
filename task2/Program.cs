e9bc202678d4e18bce2f2411321a870570c5d657
akunts <david@akunts.ru>
2022-10-13 22:22:12 +0400

    init commit

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a >= b)
+{
+    Console.WriteLine("Max: " + a);
+}
+else 
+{
+    Console.WriteLine("Max: " + b);
+}
